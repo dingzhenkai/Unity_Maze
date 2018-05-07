b5d0b0ff307e24ba969da7cb366f97848d87f494
dingzhenkai <13511577582@163.com>
2018-05-07 14:52:07 +0800

    Unity Maze

---
+++ Assets/Scripts/SimpleUI.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SimpleUI : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(0, 0, 100, 30), "replay"))
+        {
+            //Application.LoadLevel("01");
+            SceneManager.LoadScene("01");
+
+        }
+        if (GUI.Button(new Rect(0, 30, 100, 30), "pause"))
+        {
+            //Application.LoadLevel("01");
+            //SceneManager.LoadScene("01");
+            Time.timeScale = 0;
+
+        }
+        if (GUI.Button(new Rect(0, 60, 100, 30), "continue"))
+        {
+            //Application.LoadLevel("01");
+            //SceneManager.LoadScene("01");
+            Time.timeScale = 1;
+
+        }
+        if (GUI.Button(new Rect(0, 90, 100, 30), "exit"))
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+Application.Quit();
+#endif
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/followperson.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followperson : MonoBehaviour {
+
+    public Transform target;
+    public float distanceZ = 3f;
+    public float distanceY = 3f;
+    public float distanceX = 0f;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        Vector3 nextpos = target.forward * -1 * distanceZ + target.up * distanceY + target.position + target.right * distanceX;
+        this.transform.position = nextpos;
+        //this.transform.rotation = target.rotation;
+        this.transform.LookAt(target);
+    }
+}
